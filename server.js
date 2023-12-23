9fd11539c44d781b21a073d90bbee1e533d6462f
Jonaira Manha <abcd@Jonaeath.local>
2023-12-23 22:49:33 +0600

    modifyCart

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 4000;
-
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.pg0dj0q.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,31 +17,42 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     const foodsCollection = client.db("foodBlog").collection("foodItems");
+    const reviewCollection = client.db("foodBlog").collection("review");
+    const cartCollection = client.db("foodBlog").collection("carts");
+
 
     // Food Item related apis
-    app.get('/foods', async (req, res) => {
-        const result = await foodsCollection.find().toArray();
-        res.send(result);
-      });
+    app.get("/foods", async (req, res) => {
+      const result = await foodsCollection.find().toArray();
+      res.send(result);
+    });
 
-    
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+  // cart collection
+  app.post("/carts", async(req,res)=>{
+    const item = req.body;
+    // console.log(item);
+    const result = await cartCollection.insertOne(item);
+    res.send(result);
+  })
+  
   } finally {
-    
   }
 }
 run().catch(console.dir);
 
-
-
 app.get("/", async (req, res) => {
-    res.send("Food server is running");
-  });
+  res.send("Food server is running");
+});
 
 app.listen(port, () => console.log(`Food Server running on ${port}`));
- 
