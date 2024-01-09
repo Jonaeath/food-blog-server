5b0eff500636e1df17328a1745c7054a320402d5
Jonaira Manha <abcd@Jonaeath.local>
2024-01-10 01:19:00 +0600

    refector:implement verify jwt

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req,res,next) =>{
+  const authorization = req.headers.authorization;
+if(!authorization){
+  return res.status(401).send({error: true, message:'unauthorized access'})
+}
+// bearer token
+const token = authorization.split(' ')[1];
+
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+  if(err){
+    return res.status(401).send({error: true, message:'unauthorized access'})
+  }
+  req.decoded = decoded;
+  next()
+});
+
+
+}
+
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.pg0dj0q.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +47,14 @@ async function run() {
     const userCollection = client.db("foodBlog").collection("users");
     const reviewCollection = client.db("foodBlog").collection("review");
     const cartCollection = client.db("foodBlog").collection("carts");
+
+
+    app.post('/jwt', (req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+      res.send({token})
+    })
+
 
     // Users Related API
     app.post("/users", async(req,res)=>{
@@ -80,11 +108,17 @@ async function run() {
     res.send(result);
   })
 
-  app.get('/carts', async(req,res) =>{
+  app.get('/carts', verifyJWT, async(req,res) =>{
     const email = req.query.email;
     if(!email){
       res.send([]);
     }
+    
+    const decodedEmail = req.decoded.email;
+    if(email !== decodedEmail){
+    return res.status(403).send({error: true, message:'Forbidden access'})
+    }
+   
     const query = {email: email};
     const result = await cartCollection.find(query).toArray();
     res.send(result);
