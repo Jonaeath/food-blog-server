077b4864010e7f44b7a08ea081af99aa3cf9ab90
Jonaira Manha <abcd@Jonaeath.local>
2024-01-13 00:12:46 +0600

    feat:create security layer api

--- server.js
+++ server.js
@@ -76,6 +76,19 @@ async function run() {
       res.send(result)
     })
 
+    // Check Login user admin or not and create security layer
+    app.get('/users/admin/:email',verifyJWT, async(req,res)=>{
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin: false})
+      }
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      const result = {admin: user?.role === 'admin'} 
+      res.send(result) 
+    })
+
     // Admin role
     app.patch('/users/admin/:id', async(req, res)=>{
       const id = req.params.id;
